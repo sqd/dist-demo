b10d71f9bf53c0ef250cef46e7c47e610306e939
hyou <hanyourmd@gmail.com>
2019-07-24 01:04:39 +0800

    dfs

--- examples/wave.js
+++ examples/wave.js
@@ -5,12 +5,27 @@ class DFS extends BaseNode {
     }
 
     start() {
-        this.sendMsg(this.randomNeighbour(), null);
+        this.setColor('blue');
+
+        this.parent = this;
+        const n = this.randomNeighbour();
+        this.sendMsg(n, new Set([this]));
+        this.forwarded.add(n);
+    }
+
+    thisText() {
+        return this.textSVG.text();
     }
 
     onMessage(edge, msg) {
+        if (this.parent === this && msg.size == app.nodes.length) {
+            return;
+        }
+
         // Mark that we have received the message
         this.setColor('lightblue');
+        console.log(msg);
+        msg.add(this);
 
         const self = this;
 
@@ -21,18 +36,20 @@ class DFS extends BaseNode {
         // Return token immediately if it's allowed (not forwarded before,
         // AND either it's not from parent OR parent is the last left)
         if (!this.forwarded.has(edge.from) &&
-            (edge.from !== this.parent || this.forwarded.size + 1 == this.outgoing().length)) {
+            edge.from !== this.parent) {
             this.sendMsg(edge.from, msg);
             this.forwarded.add(edge.from);
         } else {
             // Find one to forward
-            this.outgoing().forEach(e => {
+            const neighbours = this.outgoing();
+            for (let i = 0; i < neighbours.length; i++) {
+                const e = neighbours[i];
                 if (e.to !== self.parent && !self.forwarded.has(e.to)) {
-                    this.sendMsg(edge.from, msg);
-                    this.forwarded.add(edge.from);
+                    this.sendMsg(e.to, msg);
+                    this.forwarded.add(e.to);
                     return;
                 }
-            });
+            }
             // No luck. Do parent
             this.sendMsg(self.parent, msg);
             this.forwarded.add(self.parent);
