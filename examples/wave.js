13ae25863f7bc024d555d8345133f1b7b72d44d4
hyou <hanyourmd@gmail.com>
2019-09-07 16:04:50 -0400

    fix up DFS

--- examples/wave.js
+++ examples/wave.js
@@ -6,35 +6,34 @@ class DFS extends BaseNode {
 
     start() {
         this.setColor('blue');
-
         this.parent = this;
-        const n = this.randomNeighbour();
-        this.sendMsg(n, new Set([this]));
-        this.forwarded.add(n);
-    }
 
-    thisText() {
-        return this.textSVG.text();
+        // Send the token (a set of nodes) to a random neighbour
+        const n = this.randomNeighbour();
+        this.forwarded.add(n);
+        this.sendMsg(n, new Set([this]));
     }
 
     onMessage(edge, msg) {
+        const self = this;
+
+        // If the token has passed everywhere and returned to the initiator, abort
         if (this.parent === this && msg.size == app.nodes.length) {
+            alert("Finished!");
             return;
         }
 
         // Mark that we have received the message
         this.setColor('lightblue');
-        console.log(msg);
         msg.add(this);
-
-        const self = this;
 
         // First contacting node becomes parent
         if (this.parent === null) {
             this.parent = edge.from;
         }
-        // Return token immediately if it's allowed (not forwarded before,
-        // AND either it's not from parent OR parent is the last left)
+
+        // Return token immediately if it's allowed: not forwarded before,
+        // AND (either it's not from parent OR parent is the last left)
         if (!this.forwarded.has(edge.from) &&
             edge.from !== this.parent) {
             this.sendMsg(edge.from, msg);
@@ -44,15 +43,18 @@ class DFS extends BaseNode {
             const neighbours = this.outgoing();
             for (let i = 0; i < neighbours.length; i++) {
                 const e = neighbours[i];
+                // If e doesn't go to the parent, and we haven't go through e before
+                // Forward through e
                 if (e.to !== self.parent && !self.forwarded.has(e.to)) {
-                    this.sendMsg(e.to, msg);
                     this.forwarded.add(e.to);
+                    this.sendMsg(e.to, msg);
                     return;
                 }
             }
+
             // No luck. Do parent
-            this.sendMsg(self.parent, msg);
             this.forwarded.add(self.parent);
+            this.sendMsg(self.parent, msg);
         }
     }
 }
